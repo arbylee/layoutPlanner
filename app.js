9eca02c5ff5de76bb109f934973f498f83f529f4
Richard Lee <rblee88@gmail.com>
2015-07-30 18:29:51 -0500

    Directional aisles and walls

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var game = new Phaser.Game(400, 490, Phaser.AUTO, '')
+// MAKE SECOND COLOR FOR WALLS SO THEY HIGHLIGHT
+var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game')
 
 function Main() {};
 
@@ -7,51 +8,77 @@ Main.prototype = {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
   },
   preload: function(){
-    this.game.load.spritesheet('aisle', 'assets/linesSprites.png', 12, 3);
+    this.game.load.spritesheet('aisle', 'assets/allAislesSprite.png', 80, 24);
+    this.game.load.spritesheet('wall', 'assets/wallsSprite.png', 80, 8);
   },
   create: function(){
+    this.game.stage.backgroundColor = "#71c5cf";
+    this.aisleInfoDelay = 200;
     this.actionDelay = 300;
-    this.moveSpeed = 50;
-    this.currentAisle = null;
+    this.moveSpeed = 150;
+    this.currentItem = null;
+
     this.aisles = this.game.add.group();
+    this.walls = this.game.add.group();
+
+    this.currentItemGroup = this.aisles;
+
+    this.aisleInfo = {};
+    for (i = 0; i < 50; i++) {
+      var aisle = new Aisle(this);
+      this.aisles.add(aisle);
+      this.aisleInfo[i] = aisle;
+    }
+    window.aisleInfo = this.aisleInfo;
+
 
     for (i = 0; i < 50; i++) {
-      this.aisles.add(new Aisle(this));
+      var wall = new Wall(this);
+      this.walls.add(wall);
     }
 
     this.cursors = this.game.input.keyboard.createCursorKeys();
-    this.spawnAisle();
+    this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
+    this.spawnItem();
   },
   update: function(){
-    this.currentAisle.body.velocity.x = 0;
-    this.currentAisle.body.velocity.y = 0;
+    this.currentItem.body.velocity.x = 0;
+    this.currentItem.body.velocity.y = 0;
     if(!this.cursors.right.isDown && !this.cursors.left.isDown){
     } else {
       if(this.cursors.left.isDown){
-        this.currentAisle.body.velocity.x -= this.moveSpeed;
+        this.currentItem.body.velocity.x -= this.moveSpeed;
       }
       if(this.cursors.right.isDown){
-        this.currentAisle.body.velocity.x += this.moveSpeed;
+        this.currentItem.body.velocity.x += this.moveSpeed;
       }
     }
 
     if(this.cursors.up.isDown){
-      this.currentAisle.body.velocity.y -= this.moveSpeed;
+      this.currentItem.body.velocity.y -= this.moveSpeed;
     }
     if(this.cursors.down.isDown){
-      this.currentAisle.body.velocity.y += this.moveSpeed;
+      this.currentItem.body.velocity.y += this.moveSpeed;
     }
 
     if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
-      this.spawnAisle();
+      this.spawnItem();
     }
 
     if(this.game.input.keyboard.isDown(Phaser.Keyboard.Q)){
-      this.currentAisle.rotateLeft();
+      this.currentItem.rotateLeft();
     }
 
     if(this.game.input.keyboard.isDown(Phaser.Keyboard.W)){
-      this.currentAisle.rotateRight();
+      this.currentItem.rotateRight();
+    }
+
+    if(this.game.input.keyboard.isDown(Phaser.Keyboard.F)){
+      this.toggleAisleType();
+    }
+
+    if(this.game.input.keyboard.isDown(Phaser.Keyboard.G)){
+      this.toggleItem();
     }
 
     if(this.game.input.keyboard.isDown(Phaser.Keyboard.P)){
@@ -61,68 +88,107 @@ Main.prototype = {
     }
     if(this.game.input.keyboard.isDown(Phaser.Keyboard.N)){
       if (this.game.time.time < this.nextChangeAisle) { return; }
-      this.nextAisle();
+      this.nextItem();
       this.nextChangeAisle = this.game.time.time + this.actionDelay;
     }
+    this.updateAisleInfo();
   },
-  spawnAisle: function(){
+  updateAisleInfo: function(){
+    var aisleElement = document.getElementById("aisleInfo");
+
+    if (this.game.time.time < this.nextAisleInfoUpdate) { return; }
+
+    aisleElement.innerHTML = "";
+    for (i = 0; i < this.aisles.length; i++) {
+      var aisle = this.aisleInfo[i];
+      if(!aisle.alive){return}
+      var selected = "";
+      if(this.currentItem.className == "Aisle" && this.aisles.getIndex(this.currentItem) == i){
+        selected = "selected"
+      };
+      aisleElement.innerHTML += "<div class=" + selected + ">" +
+        "X: " + aisle.position.x +
+        ", Y: " + aisle.position.y +
+        ", Angle: " + aisle.angle +
+        ", Has Direction Preference: " + (aisle.frame > 1) +
+        "</div>"
+    }
+    this.nextAisleInfoUpdate = this.game.time.time + this.aisleInfoDelay;
+  },
+  spawnItem: function(){
     if (this.game.time.time < this.nextSpawn) { return; }
 
-    if(this.currentAisle){
-      this.currentAisle.body.velocity.x = 0;
-      this.currentAisle.body.velocity.y = 0;
+    if(this.currentItem){
+      this.currentItem.body.velocity.x = 0;
+      this.currentItem.body.velocity.y = 0;
     }
-    var aisle = this.aisles.getFirstDead();
-    aisle.reset(200, 240);
-    this.setCurrentAisle(aisle);
+    var item = this.currentItemGroup.getFirstDead();
+    item.reset(200, 240);
+    this.setCurrentItem(item);
     this.nextSpawn = this.game.time.time + this.actionDelay;
+    return item;
   },
   previousAisle: function(){
-    var livingAisles = this.aisles.filter(function(e){return e.alive});
-    if(this.currentAisle && livingAisles.list.length > 1){
-      this.currentAisle.body.velocity.x = 0;
-      this.currentAisle.body.velocity.y = 0;
+    var livingItems = this.currentItemGroup.filter(function(e){return e.alive});
+    console.log(livingItems)
+    if(this.currentItem && livingItems.list.length > 1){
+      this.currentItem.body.velocity.x = 0;
+      this.currentItem.body.velocity.y = 0;
     }
-    var currentIndex = this.aisles.getIndex(this.currentAisle);
+    var currentIndex = this.currentItemGroup.getIndex(this.currentItem);
     var previousAisle;
     if(currentIndex == 0){
-      previousAisle = this.aisles.getAt(this.aisles.length-1);
+      previousAisle = this.currentItemGroup.getAt(this.currentItemGroup.length-1);
     } else {
-      previousAisle = this.aisles.getAt(currentIndex-1);
+      previousAisle = this.currentItemGroup.getAt(currentIndex-1);
     }
     if(!previousAisle.alive){
-      ai = livingAisles;
-      var index = livingAisles.getIndex(livingAisles.list[livingAisles.list.length-1]);
-      bar = this.aisles.getAt(index)
-      this.setCurrentAisle(this.aisles.getAt(index));
+      var index = livingItems.getIndex(livingItems.list[livingItems.list.length-1]);
+      this.setCurrentItem(this.currentItemGroup.getAt(index));
     } else {
-      this.setCurrentAisle(previousAisle);
+      this.setCurrentItem(previousAisle);
     }
+    this.updateAisleInfo();
   },
-  nextAisle: function(){
-    var livingAisles = this.aisles.filter(function(e){return e.alive});
-    if(!this.currentAisle || !livingAisles.list.length > 1){ return }
+  nextItem: function(){
+    var livingAisles = this.currentItemGroup.filter(function(e){return e.alive});
+    if(!this.currentItem || !livingAisles.list.length > 1){ return }
 
-    var nextAisle;
-    var currentIndex = this.aisles.getIndex(this.currentAisle);
-    if(currentIndex == this.aisles.length-1){
-      nextAisle = this.aisles.getAt(0);
+    var nextItem;
+    var currentIndex = this.currentItemGroup.getIndex(this.currentItem);
+    if(currentIndex == this.currentItemGroup.length-1){
+      nextItem = this.currentItemGroup.getAt(0);
     } else {
-      nextAisle = this.aisles.getAt(currentIndex+1);
+      nextItem = this.currentItemGroup.getAt(currentIndex+1);
     }
-    if(!nextAisle.alive){
-      nextAisle = this.aisles.getAt(0);
+    if(!nextItem.alive){
+      nextItem = this.currentItemGroup.getAt(0);
     }
-    this.setCurrentAisle(nextAisle);
+    this.setCurrentItem(nextItem);
+    this.updateAisleInfo();
   },
-  setCurrentAisle: function(aisle){
-    if(this.currentAisle){
-      this.currentAisle.body.velocity.x = 0;
-      this.currentAisle.body.velocity.y = 0;
-      this.currentAisle.frame = 0;
+  setCurrentItem: function(item){
+    if(this.currentItem){
+      this.currentItem.body.velocity.x = 0;
+      this.currentItem.body.velocity.y = 0;
+      this.currentItem.frame = 0 + this.currentItem.offset;
     }
-    this.currentAisle = aisle;
-    this.currentAisle.frame = 1;
+    this.currentItem = item;
+    this.currentItem.frame = 1 + this.currentItem.offset;
+  },
+  toggleAisleType: function(){
+    this.currentItem.toggleType();
+  },
+  toggleItem: function(){
+    if (this.game.time.time < this.nextToggleItem) { return; }
+    if(this.currentItem.className == "Aisle"){
+      this.currentItemGroup = this.walls;
+    } else {
+      this.currentItemGroup = this.aisles;
+    }
+    var item = this.spawnItem();
+    this.setCurrentItem(item);
+    this.nextToggleItem = this.game.time.time + this.actionDelay;
   }
 };
 
@@ -136,6 +202,9 @@ var Aisle = function (state) {
   this.exists = false;
   this.alive = false;
   this.anchor.setTo(0.5, 0.5);
+  this.actionDelay = 200;
+  this.className = "Aisle";
+  this.offset = 0;
 };
 
 Aisle.prototype = Object.create(Phaser.Sprite.prototype);
@@ -147,13 +216,64 @@ Aisle.prototype.create = function(){
 Aisle.prototype.update = function () {
 };
 
-Aisle.prototype.rotateLeft = function(){
-  this.angle -= 2;
-}
-Aisle.prototype.rotateRight = function(){
-  this.angle += 2;
+Aisle.prototype.toggleType = function(){
+  if (this.game.time.time < this.nextToggleType) { return; }
+  if(this.offset == 0){
+    this.offset = 2;
+  } else {
+    this.offset = 0;
+  }
+  this.updateFrame();
+  this.nextToggleType = this.game.time.time + this.actionDelay;
 }
 
+Aisle.prototype.updateFrame = function(){
+  this.frame = 1 + this.offset;
+}
+
+Aisle.prototype.rotateLeft = function(){
+  if (this.game.time.time < this.nextRotate) { return; }
+  this.angle -= 45;
+  this.nextRotate = this.game.time.time + this.actionDelay;
+}
+Aisle.prototype.rotateRight = function(){
+  if (this.game.time.time < this.nextRotate) { return; }
+  this.angle += 45;
+  this.nextRotate = this.game.time.time + this.actionDelay;
+}
+
+
+var Wall = function (state) {
+  this.gameState = state;
+  this.game = state.game;
+  Phaser.Sprite.call(this, this.game, 0, 0, 'wall');
+  this.game.physics.arcade.enable(this);
+  this.exists = false;
+  this.alive = false;
+  this.anchor.setTo(0.5, 0.5);
+  this.actionDelay = 200;
+  this.className = "Wall";
+};
+
+Wall.prototype = Object.create(Phaser.Sprite.prototype);
+Wall.prototype.constructor = Wall;
+
+Wall.prototype.create = function(){
+}
+
+Wall.prototype.update = function () {
+};
+
+Wall.prototype.rotateLeft = function(){
+  if (this.game.time.time < this.nextRotate) { return; }
+  this.angle -= 45;
+  this.nextRotate = this.game.time.time + this.actionDelay;
+}
+Wall.prototype.rotateRight = function(){
+  if (this.game.time.time < this.nextRotate) { return; }
+  this.angle += 45;
+  this.nextRotate = this.game.time.time + this.actionDelay;
+}
 
 
 
